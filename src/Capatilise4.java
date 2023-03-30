cddf9ec5b08ae531b835c04fb80281f2ea898b8c
Ram Thapa <thapa.ram9590@gmail.com>
2023-03-31 00:09:25 +1000

    Finished prac 4 tasks

---
+++ src/Capatilise4.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Capatilise4 {
+    /* This program will capitalise the first letter of each word in a sentence. */
+    public static void main(String[] args) {
+        System.out.println("Please input a string to capitalise: ");
+        Scanner input = new Scanner(System.in);
+        String sentence = input.nextLine();
+        System.out.println(capitalise(sentence));
+
+    }
+
+    public static String capitalise(String sentence) {
+        String[] words = sentence.split(" ");
+        StringBuilder sentenceBuilder = new StringBuilder(sentence);
+        for (String word : words) {
+            String firstLetter = word.substring(0, 1);
+            String restOfWord = word.substring(1);
+            String capitalisedWord = firstLetter.toUpperCase() + restOfWord;
+            sentenceBuilder.append(capitalisedWord).append(" ");
+        }
+        sentence = sentenceBuilder.toString();
+        return sentence;
+    }
+}
---
+++ src/RollingDice4.java
@@ -0,0 +1,38 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RollingDice4 {
+    public static void main(String[] args) {
+        /* */
+        System.out.println("Please type the total you want on dice");
+        Scanner input = new Scanner(System.in);
+        int total = input.nextInt();
+        if (total < 2 || total > 12) {
+            throw new IllegalArgumentException("Impossible total for a pair of dice.");
+        }
+        System.out.println("It took " + diceRoller(total) + " rolls to get " + total + " on both dice.");
+
+    }
+
+    public static int diceRoller(int n){
+
+        Random rand = new Random();
+        int die1, die2, rolls = 0;
+
+        do {
+            die1 = rand.nextInt(6) + 1;
+            die2 = rand.nextInt(6) + 1;
+            rolls++;
+        } while (die1 + die2 != n);
+
+        return rolls;
+
+    }
+}
+
+
+
+
+
+
+
